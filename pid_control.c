1021d2e96abab835304c94190392f852d98859af
kimdj98 <kimdj98g@gmail.com>
2022-10-18 16:37:48 +0900

    modify pid_control.c pid_control function by reference to by value

--- pid_control.c
+++ pid_control.c
@@ -54,7 +54,6 @@ void funENCODER_A()
 
 }
 
-
 void funENCODER_B()
 {
     encA = digitalRead(ENCODER_A);
@@ -91,6 +90,7 @@ void funENCODER_B()
 
 }
 
+// function for read_position of motor
 float read_position()
 {
     return redGearPosition;
@@ -109,8 +109,8 @@ void set_amplifier(float actu_signal) // integer signal?
         softPWmWrite(MOTOR2, -actu_signal);
     }
 }
-
-
+// function prototypes
+void pid_control(int reference, int Pgain, int Igain, int Dgain, float T, float* itae);
 int main() {
 
     wiringPiSetupGpio();
@@ -137,27 +137,27 @@ int main() {
     for (int i = 0; i < n; i ++) {
         printf("Enter reference motor position: ");
         scanf("%d", &reference);
-        pid_control(&reference, &Pgain, &Igain, &Dgain, &T, &itae);
+        pid_control(reference, Pgain, Igain, Dgain, T, &itae);
         printf("measured itae(iteration: %d): %f\n", i+1, itae);
     }
     printf("PerformanceITAE: %f\n", itae);
     return 0;
 }
 
-void pid_control(int* reference, int* Pgain, int* Igain, int* Dgain, float* T, float* itae) { // get variables by reference
+void pid_control(int reference, int Pgain, int Igain, int Dgain, float T, float* itae) { // get variables by reference
     float m, m1=0, e, e1=0, e2=0; // declare variable for ms and errors
     float G1, G2, G3; // coefficients for e_n, e_(n-1), e_(n-2)
     float c;
     // setting coefficients for e_n, e_(n-1), e_(n-2)
-    G1 = ((float)(*Pgain) + (float)(*Igain) *(*T) + (float)(*Dgain) / (*T));
-    G2 = -((float)(*Pgain) + 2 * (float)(*Dgain) / (*T));
-    G3 = ((float)(*Dgain))/(*T);
+    G1 = ((float)Pgain + (float)Igain *T + (float)Dgain / T);
+    G2 = -((float)Pgain + 2 * (float)Dgain / T);
+    G3 = ((float)Dgain)/T;
 
     unsigned int startTime = millis(); // measure time for T = T
     while (1) {
         unsigned int innerStartTime = millis();
-        c = read_position(motor1);
-        e = (float)(*reference) - c;
+        c = read_position();
+        e = (float)reference - c;
         m = m1 + G1 * e + G2 * e1 + G3 * e2; // update actuating signal
         set_amplifier(m);
         e2 = e1; //
@@ -165,15 +165,12 @@ void pid_control(int* reference, int* Pgain, int* Igain, int* Dgain, float* T, f
         m1 = m; //
 
         unsigned int endTime = millis();
-        while ((endTime - innerStartTime)< (*T) * 1000) { // stop process until T seconds arrive
+        while ((endTime - innerStartTime)< T * 1000) { // stop process until T seconds arrive
             endTime = millis();
         } // delay
-        *itae += (endTime - startTime) * abs(e) * (*T);
+        *itae += (endTime - startTime) * abs(e) * T;
 
         if ((e-e1) < 1e-10 && (e-e2) < 1e-10) // break while loop if steady state error occured
             break;
     }
 }
-
-float ITAE
-
