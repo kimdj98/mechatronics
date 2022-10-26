1a04f4867dfa4bb1cb8bf83a5e645e6868e8b0ee
kimdj98 <kimdj98g@gmail.com>
2022-10-26 19:08:47 +0900

    add comments

--- GET_GAINVALUE.c
+++ GET_GAINVALUE.c
@@ -11,14 +11,15 @@
 #define MOTOR2 26
 #define PULSE 21
 
-FILE* fpt;
+FILE* fpt; // file pointer to save gains and itae
 
 int encA;
 int encB;
-int encoderPosition = 0;
-float redGearPosition = 0;
+int encoderPosition = 0; // encoder position out of 216
+float redGearPosition = 0; // motor position in rounds
 
 //###################### configuration ######################
+// configuration for tuning code
 int tuning_iteration = 10;
 int tuning_reference = 5;
 
@@ -34,22 +35,22 @@ void funENCODER_A()
 {
 	encA = digitalRead(ENCODER_A);
 	encB = digitalRead(ENCODER_B);
-	if (encA == HIGH)
+	if (encA == HIGH)    // A is rising
 	{
-		if (encB == LOW)
+		if (encB == LOW)    // If B is low, turn right
 			encoderPosition++;
-		else
+		else                // if B is High, turn left
 			encoderPosition--;
 	}
-	else
+	else                // A is falling
 	{
 		if (encB == LOW)
-			encoderPosition--;
+			encoderPosition--;  // if B is low, turn left
 		else
-			encoderPosition++;
+			encoderPosition++;  // if B is High, turn right
 	}
 
-	redGearPosition = ((float)encoderPosition) / ENC2REDGEAR;
+	redGearPosition = ((float)encoderPosition) / ENC2REDGEAR; // calculate Gear position
 //	printf("funcEncoder_A Result: encPos:%d gearPos:%f\n", encoderPosition, redGearPosition); // uncomment this to see motor position
 }
 
@@ -58,22 +59,22 @@ void funENCODER_B()
 {
 	encA = digitalRead(ENCODER_A);
 	encB = digitalRead(ENCODER_B);
-	if (encB == HIGH)
+	if (encB == HIGH)   // B is rising
 	{
-		if (encA == LOW)
+		if (encA == LOW)    //if A is low, turn left
 			encoderPosition--;
-		else
+		else                //If A is high, turn right
 			encoderPosition++;
 	}
-	else
+	else                // B is falling
 	{
-		if (encA == LOW)
+		if (encA == LOW)    //If A is Low, turn right
 			encoderPosition++;
 
-		else
+		else                //If A is High, turn left
 			encoderPosition--;
 	}
-	redGearPosition = ((float)encoderPosition) / ENC2REDGEAR;
+	redGearPosition = ((float)encoderPosition) / ENC2REDGEAR;//Calculate the Gear Position
 //	printf("funcEncoder_A Result: encPos:%d gearPos:%f\n", encoderPosition, redGearPosition); // uncomment this to see motor position in realtime
 
 }
@@ -94,10 +95,10 @@ int main()
 	softPwmCreate(MOTOR1, 0, 100);
 	softPwmCreate(MOTOR2, 0, 100);
 
-	wiringPiISR(ENCODER_A, INT_EDGE_BOTH, funENCODER_A);
+	wiringPiISR(ENCODER_A, INT_EDGE_BOTH, funENCODER_A); // interrupt
 	wiringPiISR(ENCODER_B, INT_EDGE_BOTH, funENCODER_B);
 
-    int pulse = 0; // pulse variable
+    int pulse = 0; // set pulse = 0
 
 //#################     tuning code     ########################
 //    printf("mode: %c\n", mode);
@@ -115,7 +116,7 @@ int main()
     printf("Enter number of iterations: ");
     scanf("%d", &test_iteration);
 
-    int references[10] = {0,};
+    int references[10] = {0,}; // array for saving reference value
 
     for (int i = 0; i < test_iteration; i++) {
         printf("Enter reference number: %d", i+1);
@@ -126,7 +127,7 @@ int main()
         while(1)
         {
             pulse = digitalRead(PULSE);
-            if (pulse == HIGH) {
+            if (pulse == HIGH) { //If program gets a pulse, start PID control
                 itae += PID_Control(references[i], Pgain, Igain, Dgain); // cumulate itae for each iteration
                 printf("itae(iteration: %d): %f\n", i, itae);
                 break;
@@ -140,7 +141,7 @@ int main()
     softPwmWrite(MOTOR1, 0);
     softPwmWrite(MOTOR2, 0);
     return 0;
-//################    stop motor1, 2     #########################
+//################    stop motor 1,2     #########################
 }
 
 
@@ -154,39 +155,39 @@ double PID_Control(int reference, float Pgain, float Igain, float Dgain)
 	float error_1 = 0, error_2 = 0;
 	float T = 0.01;
     double itae = 0;
-	float G1 = (Pgain + Igain * T + Dgain / T);
+	float G1 = (Pgain + Igain * T + Dgain / T); // Discretize the equation
 	float G2 = -(Pgain + 2 * Dgain / T);
 	float G3 = Dgain / T;
 
-	unsigned int startTime = millis();
+	unsigned int startTime = millis(); //start recording time to calculate 'T' term inside ITAE integrand
 	while (1)
 	{
-		unsigned int inner_startTime = millis();
+		unsigned int inner_startTime = millis(); //start recording to measure the sampling time
 
 		curr_pos = redGearPosition;
 		error = (float)reference - curr_pos;
 
-		m = m1 + G1 * error + G2 * error_1 + G3 * error_2;
+		m = m1 + G1 * error + G2 * error_1 + G3 * error_2; // using the difference equation, get new actuating signal
 
 		//printf("Error Value: %f\n", error);
 		//printf("Actuating signal Value: %f\n", m);
 
-		set_amplifer(m, error);
+		set_amplifer(m, error); // rotate the motor with actuating signal
 
-		error_2 = error_1;
-		error_1 = error;
+		error_2 = error_1; // update previous error
+		error_1 = error; // update previous error
 		m1 = m;
 
 		unsigned int inner_endTime = millis();
 
-		while (inner_endTime - inner_startTime < T * 1000)
+		while (inner_endTime - inner_startTime < T * 1000) // delay until sampling time passes
 		{
 			inner_endTime = millis();
 		}
-		itae += (inner_endTime - startTime) / 1000 * fabs(error) * T;
+		itae += (inner_endTime - startTime) / 1000 * fabs(error) * T; // accumulate the ITAE value
 
 		unsigned int endtime = millis();
-		if (endtime - startTime >= 4000) // perform pid control for 7 seconds
+		if (endtime - startTime >= 4000) // perform pid control for 4 seconds
 		{
 			softPwmWrite(MOTOR1, 0);
 			softPwmWrite(MOTOR2, 0);
@@ -199,14 +200,14 @@ double PID_Control(int reference, float Pgain, float Igain, float Dgain)
     return itae;
 }
 
-void PID_Tuning(int reference, char mode, int iteration, float delta ,float Pgain, float Igain, float Dgain) {
+void PID_Tuning(int reference, char mode, int iteration, float delta ,float Pgain, float Igain, float Dgain) { // tuning
     float min_itae = 1e+20; // infinity
-    float min_Pgain = 0.0;
-    float min_Igain = 0.0;
-    float min_Dgain = 0.0;
+    float min_Pgain = 0.0; // initial Pgain
+    float min_Igain = 0.0; // initial Igain
+    float min_Dgain = 0.0; // initial Dgain
 
     switch(mode){
-        case 'P' :
+        case 'P' : // mode P changes delta for (iteration) times {P, P + 1del, P + 2del, P + 3del, P + 4del, ... }
             printf("Tuning P_gain\n");
             for (int i = 0; i < iteration; i++) {
                 double itae = PID_Control(reference, Pgain, Igain, Dgain);
@@ -224,7 +225,7 @@ void PID_Tuning(int reference, char mode, int iteration, float delta ,float Pgai
             printf("==================================================\n");
             printf("minimum itae: %lf (Pgain: %f, Igain: %f, Dgain: %f\n", min_itae, min_Pgain, min_Igain, min_Dgain);
             break;
-        case 'I' :
+        case 'I' : // mode I changes delta for (iteration) times {I, I + 1del, I + 2del, I + 3del, I + 4del, ... }
             printf("Tuning I_gain\n");
             for (int i = 0; i < iteration; i++) {
                 double itae = PID_Control(reference, Pgain, Igain, Dgain);
@@ -242,7 +243,7 @@ void PID_Tuning(int reference, char mode, int iteration, float delta ,float Pgai
             printf("==================================================\n");
             printf("minimum itae: %lf (Pgain: %f, Igain: %f, Dgain: %f\n", min_itae, min_Pgain, min_Igain, min_Dgain);
             break;
-        case 'D' :
+        case 'D' : // mode D changes delta for (iteration) times {D, D + 1del, D + 2del, D + 3del, D + 4del, ... }
             printf("Tuning D_gain\n");
             for (int i = 0; i < iteration; i++) {
                 double itae = PID_Control(reference, Pgain, Igain, Dgain);
@@ -265,7 +266,7 @@ void PID_Tuning(int reference, char mode, int iteration, float delta ,float Pgai
     }
 }
 
-void set_amplifer(float m, float error)
+void set_amplifer(float m, float error) // motor control code
 {
 	if (error > 0)
 	{
